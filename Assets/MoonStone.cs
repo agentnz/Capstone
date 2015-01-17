5a767c63ae34b589a41c04a6cc150a84973122f4
diggoran <diggoran@gmail.com>
2015-01-16 20:06:49 -0500

    Started coding sun and moon stones

---
+++ Assets/MoonStone.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections;
+
+public class MoonStone : MonoBehaviour {
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("Moon Stone active!");
+		other.GetComponentsInChildren
+	}
+}
