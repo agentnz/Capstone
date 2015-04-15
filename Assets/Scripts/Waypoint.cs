7cd0f10203d9ebc96686721ab3fd7fc67da10d3b
agentnz <npz2dw@virginia.edu>
2015-04-15 03:59:49 -0400

    ready for testing

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -7,11 +7,20 @@ public class Waypoint : MonoBehaviour {
 	private int i = 0;
 	private bool gameWon = false;
 	public GameObject player;
+	public GameObject landmines;
 	
 	void OnTriggerEnter(Collider other) {
 		transform.position = waypoints [i].position;
 		i++;
+		if (i == waypoints.Length - 1) {
+			foreach (CapsuleCollider collider in landmines.GetComponentsInChildren<CapsuleCollider>()) {
+				collider.enabled = true;
+			}
+		}
 		if (i == waypoints.Length) {
+			foreach (CapsuleCollider collider in landmines.GetComponentsInChildren<CapsuleCollider>()) {
+				collider.enabled = false;
+			}
 			gameWon = true;
 			player.GetComponent<CharacterMotor>().movement.gravity = 0;
 		}
