47016e44f0b2767f0a437d23bfbb9d52b585bb77
diggoran <diggoran@gmail.com>
2015-04-14 21:42:57 -0400

    beating the game

--- Assets/Scripts/Moodswing.cs
+++ Assets/Scripts/Moodswing.cs
@@ -2,10 +2,8 @@ using UnityEngine;
 using System.Collections;
 
 public class Moodswing : MonoBehaviour {
-	
-	//public GameObject objectHolder;
+
 	public ObjectLists objects;
-	//private ObjectLists objects = objectHolder.GetComponent<ObjectLists>();
 
 	void OnTriggerEnter(Collider other) {
 		foreach (Light light in objects.lightsOn)
@@ -16,10 +14,5 @@ public class Moodswing : MonoBehaviour {
 			mesh.enabled = true;
 		foreach (MeshRenderer mesh in objects.meshesOff)
 			mesh.enabled = false;
-		/*foreach (Object obj in objsOn ) {
-			Light light = obj as Light;
-			if (light != null)
-				light.enabled = true;
-		}*/
 	}
 }
---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Waypoint : MonoBehaviour {
+
+	public Transform[] waypoints;
+	private int i = 0;
+	private bool gameWon = false;
+	public GameObject player;
+	
+	void OnTriggerEnter(Collider other) {
+		if (i < waypoints.Length) {
+			transform.position = waypoints [i].position;
+			i++;
+		} else {
+			gameWon = true;
+			player.GetComponent<CharacterMotor>().movement.gravity = 0;
+		}
+	}
+
+	void Update() {
+		if (gameWon) {
+			player.transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+		}
+	}
+}
